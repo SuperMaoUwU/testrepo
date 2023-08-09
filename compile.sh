e6316f527b7cc4f2914aa12f96e22b80213ae0e2
SuperMaoUwU <126746935+SuperMaoUwU@users.noreply.github.com>
2023-08-10 00:49:38 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-x86_64-w64-mingw32-gcc `sdl2-config --cflags` `pkg-config --cflags --libs webkit2gtk-4.0` -o sillytavui.exe main.c
+i686-w64-mingw32-gcc -I/usr/include/SDL2 `pkg-config --cflags --libs webkit2gtk-4.0` -o sillytavui.exe main.c
